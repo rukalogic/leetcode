6c568170a492e66bb6fe84a76ce906d09f80331a
rukalogic <torukaanthony@yahoo.com>
2025-07-17 19:50:54 +0100

    Time: 12 ms (68.77%) | Memory: 31.4 MB (82.54%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        setform = {}
-        for i in range(len(nums)):
-            setform[nums[i]] = i
-        return nums != list(setform.keys())
+        return len(nums) != len(list(set(nums)))
 
 
         
