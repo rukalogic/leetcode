ac1c03e1a268d9e6ab16ce2050e23e5dc1c2b414
rukalogic <torukaanthony@yahoo.com>
2025-07-18 23:32:18 +0100

    Time: 0 ms (100.00%) | Memory: 19.1 MB (8.06%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,13 +4,9 @@ class Solution:
         no_of_unique = 0
         for i in range(len(nums)):
             if nums[i] == current:
-                nums[i] = ''
+                nums[i] = '-'
             else:
                 current = nums[i]
                 no_of_unique += 1
-        nums.sort(key= lambda x: (isinstance(x,str), x))
+        nums[:] = [x for x in nums if isinstance(x, int)]
         return no_of_unique
-
-
-
-        
