5f58440ef44da9ff1270852da49089fd26ed3322
rukalogic <torukaanthony@yahoo.com>
2025-07-19 23:09:15 +0100

    Time: 3 ms (44.79%) | Memory: 18.1 MB (12.65%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -2,7 +2,7 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a_dec = 0
         b_dec = 0
-        answer = []
+        answer =''
         for i in range(len(a)):
             a_dec += int(a[i])
             if i != len(a)-1:
@@ -13,10 +13,8 @@ class Solution:
                 b_dec *= 2
         sum = a_dec + b_dec
         while sum > 0:
-             answer.append(str(sum%2))
+             answer = str(sum%2) + answer
              sum //= 2
         if not answer:
             answer = '0'
-            return answer 
-        answer2 = ''.join(answer)[::-1]
-        return answer2
+        return answer
