73eca36014103c40f55c82ae3bd4e3179e957ed0
rukalogic <torukaanthony@yahoo.com>
2025-07-22 14:02:27 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.29%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num < 2:
+            return True
+        sqrt = num // 2
+        while True:
+            new_sqrt = (sqrt + num / sqrt) /2
+            if abs(new_sqrt - sqrt) < 1e-6:
+                break
+            sqrt = new_sqrt
+        return round(sqrt, 5) == int(sqrt)
+            
