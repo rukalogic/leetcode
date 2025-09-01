5a5e3d2b038cb6a9a6f6b615bb2d9f443345186f
rukalogic <torukaanthony@yahoo.com>
2025-09-01 13:37:49 +0100

    Time: 28 ms (6.50%) | Memory: 18.1 MB (10.93%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
@@ -4,8 +4,8 @@ class Solution:
             if i == 0:
                 minm = nums[i]
                 maxi = nums[i]
-            minm = min(minm, nums[i])
-            maxi = max(maxi, nums[i])
+            minm = minm if minm < nums[i] else nums[i]
+            maxi = maxi if maxi > nums[i] else nums[i]
         for num in nums:
             if num != maxi and num != minm:
                 return num
